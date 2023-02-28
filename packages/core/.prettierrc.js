91e6fb51a0b8349d968bfa05cf5686af3c083788
Dom Webber <dom.webber@hotmail.com>
2023-02-28 22:28:11 +0000

    added package-level eslint, tsconfig and prettier

---
+++ packages/core/.prettierrc.js
@@ -0,0 +1,15 @@
+/**
+ * Envoy CMS.
+ *
+ * @since     1.0.0
+ * @module    Envoy
+ * @copyright 2023 Dom Webber, Relucent Ltd & Contributors
+ * @author    Dom Webber <dom.webber@hotmail.com> (https://github.com/domwebber)
+ * @author    Relucent Ltd <hello@relucent.dev> (https://relucent.dev)
+ * @author    Contributors (https://github.com/envoy/graphs/contributors)
+ * @see       {@link https://github.com/domwebber/envoy}
+ */
+
+module.exports = {
+  ...require("../../.prettierrc.js"),
+};
