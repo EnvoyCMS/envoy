932e2c55805b2ea24f25875b55599464ae3d865d
Dom Webber <dom.webber@hotmail.com>
2023-02-28 23:18:11 +0000

    moved to imports

--- packages/core/__tests__/core.test.js
+++ packages/core/__tests__/core.test.js
@@ -1,7 +1,18 @@
-'use strict';
+/**
+ * Envoy CMS.
+ *
+ * @since     1.0.0
+ * @module    Envoy/Core
+ * @license   MIT
+ * @copyright 2023 Dom Webber, Relucent Ltd & Contributors
+ * @author    Dom Webber <dom.webber@hotmail.com> (https://github.com/domwebber)
+ * @author    Relucent Ltd <hello@relucent.dev> (https://relucent.dev)
+ * @author    Contributors (https://github.com/envoy/graphs/contributors)
+ * @see       {@link https://github.com/domwebber/envoy}
+ */
 
-const core = require('..');
-const assert = require('assert').strict;
+import core from "../src";
+import assert from "assert/strict";
 
-assert.strictEqual(core(), 'Hello from core');
+assert.strictEqual(core(), "Hello from core");
 console.info("core tests passed");
